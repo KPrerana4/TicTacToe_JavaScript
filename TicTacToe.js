24bda9656c9896e541ee75fd04dfce8e7878e240
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-10 00:32:29 +0530

    add function printWinner

--- TicTacToe.js
+++ TicTacToe.js
@@ -4,12 +4,10 @@ class TicTacToe
 {
     constructor()
     {
-        this.players = new Array(2);
-        this.players[0] = new Player('x',0);
-        this.players[1] = new Player('O',1);
+        this.players = [new Player('x',0) , new Player('O',1)];
         this.winningConditions = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
         this.board = new Board(this.winningConditions);
-        this.currentPlayer = this.players[1];
+        this.currentPlayer = this.players[0];
     }
 
     start()
@@ -17,16 +15,23 @@ class TicTacToe
         let win;
         while(!win && !this.board.isBoardFilled()){
             win = this.play();
+            this.switchPlayer();
         }
+        this.switchPlayer();
+        this.printWinner(win);
+    }
+
+    printWinner(win)
+    {
         let sentence = "Player " + (this.currentPlayer.playerNo + 1);
-        if(win == false)
-            sentence = "No one";
+        if(win == false){
+             sentence = "No one";
+        }
         console.log(sentence + " won");
     }
 
     play()
     {
-        this.switchPlayer();
         let position = this.getValidPosition();
         this.board.placeSymbol(position, this.currentPlayer.symbol);
         this.currentPlayer.addPosition(position);
@@ -43,8 +48,7 @@ class TicTacToe
     getValidPosition()
     {
         let valid, position;
-        do
-        {
+        do{
             position = this.currentPlayer.input();
             valid = this.board.validatePosition(position);
         }

6b12bf62d28bce64a95c9df8d0e85ba1ab443d7d
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-19 08:40:44 +0530

    change functions

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,40 +1,34 @@
-const Player = require('./Player');
-const Board = require('./Board');
 class TicTacToe
 {
-    constructor()
+    constructor(player1Symbol,  player2Symbol)
     {
-        this.players = [new Player('x',0) , new Player('O',1)];
+        this.players = [new Player(player1Symbol,0) , new Player(player2Symbol,1)];
         this.winningConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
         this.board = new Board(this.winningConditions);
         this.currentPlayer = this.players[0];
+        this.win = false;
     }
 
-    start()
+    start(position)
     {
-        let win;
-        while(!win && !this.board.isBoardFilled()){
-            win = this.play();
-            this.switchPlayer();
-        }
+        if(this.win)
+            return;
+        this.win = this.play(position);
+        this.printWinner(this.win);
         this.switchPlayer();
-        this.printWinner(win);
     }
 
     printWinner(win)
     {
-        let sentence = "Player " + (this.currentPlayer.playerNo + 1);
-        if(win == false){
-             sentence = "No one";
-        }
-        console.log(sentence + " won");
+        if(win)
+            document.getElementById("result").innerHTML = "You Won";
+        else if(this.board.isBoardFilled())
+            document.getElementById("result").innerHTML = "No one Won";
     }
 
-    play()
+    play(position)
     {
-        let position = this.getValidPosition();
         this.board.placeSymbol(position, this.currentPlayer.symbol);
-        this.board.printBoard();
         return this.board.winCheck();
     }
 
@@ -43,24 +37,4 @@ class TicTacToe
         let nextPlayerNo = 1 - this.currentPlayer.playerNo;
         this.currentPlayer = this.players[nextPlayerNo];
     }
-
-    getValidPosition()
-    {
-        let valid, position;
-        do{
-            position = this.getPosition();
-            valid = this.board.validatePosition(position);
-        }
-        while(!valid);
-        return position;
-    }
-
-    getPosition()
-    {
-        let sentence = "Player " + (this.currentPlayer.playerNo+1) + " position:";
-        return parseInt(require('readline-sync').question(sentence)) - 1;
-    }
-
 }
-
-module.exports = TicTacToe;

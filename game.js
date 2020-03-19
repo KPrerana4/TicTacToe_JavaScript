f562bb6aab0e2cf74e462bb5536609b02b5912c2
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-19 13:27:50 +0530

    game controller

--- game.js
+++ game.js
@@ -1,52 +1,53 @@
-var count = 0;
-var win = false;
-function game(buttonId)
+var player1Symbol, player2Symbol;
+var ticTacToe;
+
+for(let position = 0; position < 9 ; position++)
 {
-    if(win == true)
-       return;
-    if(setSymbol(buttonId)){
-        win = winCheck(getGrid());
-        //alert(win);
-        printWinner();
-    }
+    document.getElementById(position).addEventListener("click", game);
 }
 
-function setSymbol(buttonId)
+function game()
 {
-    let button = document.getElementById(buttonId);
-    if(button.innerHTML == ' '){
-        button.innerHTML = (count % 2 == 0) ? 'X' : 'O';
-        count++;
-        return true
+    let position = event.target.id;
+    if(document.getElementById(position).innerHTML != ' ')
+        return;
+    ticTacToe.start(position);
+}
+
+function start()
+{
+    getValidSymbols();
+    ticTacToe = new TicTacToe(player1Symbol, player2Symbol);
+    document.getElementById("board").style.display = "block";
+}
+
+function getValidSymbols()
+{
+    player1Symbol = document.getElementById("player1Symbol").value;
+    player2Symbol = document.getElementById("player2Symbol").value;
+    if(areSymbolsInvalid())
+        [player1Symbol, player2Symbol] = ['X', 'O'];
+    document.getElementById("player1Symbol").disabled = true;
+    document.getElementById("player2Symbol").disabled = true;
+}
+
+function areSymbolsInvalid()
+{
+    if(player2Symbol == '' || player1Symbol == '' || player2Symbol == player1Symbol)
+    {
+        let sentence = "Both symbols has to be valid, as they are invalid 'X' and 'O' are choosen";
+        document.getElementById("message").innerHTML = sentence;
+        return true;
     }
     return false;
 }
 
-function winCheck(grid)
+function reset()
 {
-    let winningConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-    return winningConditions.some((element) => {
-        return ((grid[element[0]] == grid[element[1]]) && (grid[element[1]] == grid[element[2]]) && (grid[element[0]] != ' '));
-    },grid);
-}
-
-function getGrid()
-{
-    let grid = [];
-    for(let index = 0; index < 9; index++)
-        grid[index] = document.getElementById(index).innerHTML;
-    return grid;
-}
-
-function isBoardFilled(grid)
-{
-    return !grid.includes(' ');
-}
-
-function printWinner()
-{
-    if(win)
-        document.getElementById("result").innerHTML = "You Won";
-    else if(isBoardFilled(getGrid()))
-        document.getElementById("result").innerHTML = "No one Won";
+    for(let position = 0; position < 9; position++)
+    {
+        document.getElementById(position).innerHTML = " ";
+    }
+    ticTacToe = new TicTacToe(player1Symbol, player2Symbol);
+    document.getElementById("result").innerHTML = "";
 }

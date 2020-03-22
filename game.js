4518a9c129e0818273c6afa49b754fe43d9af6b2
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-22 23:03:51 +0530

    change display statement

--- game.js
+++ game.js
@@ -6,22 +6,15 @@ for(let position = 0; position < 9 ; position++)
     document.getElementById(position).addEventListener("click", game);
 }
 
-function game()
-{
-    let position = event.target.id;
-    if(document.getElementById(position).innerHTML != ' ')
-        return;
-    ticTacToe.start(position);
-}
-
-function start()
-{
-    if(!areSymbolsValid())
-        return;
-    disableFields();
-    ticTacToe = new TicTacToe(player1Symbol, player2Symbol);
-    document.getElementById("board").style.display = "block";
-    document.getElementById("symbol").innerHTML = "Current player symbol:"+player1Symbol;
+function validate(id){
+    let symbol = document.getElementById(id);
+    player1Symbol = document.getElementById("player1Symbol").value;
+    player2Symbol = document.getElementById("player2Symbol").value
+    if((player1Symbol == player2Symbol) || (['',' '].includes(symbol.value))){
+        symbol.value = '';
+        symbol.focus();
+        document.getElementById("message").innerHTML = "Enter valid symbols";
+    }
 }
 
 function areSymbolsValid()
@@ -35,11 +28,29 @@ function areSymbolsValid()
     return true;
 }
 
+function start()
+{
+    if(!areSymbolsValid())
+        return;
+    disableFields();
+    ticTacToe = new TicTacToe(player1Symbol, player2Symbol);
+    document.getElementById("board").style.display = "block";
+    document.getElementById("playerDetails").innerHTML = "Current player number:1 symbol:"+player1Symbol;
+}
+
 function disableFields()
 {
     document.getElementById("message").style.display = 'none';
     document.getElementById("player1Symbol").disabled = true;
     document.getElementById("player2Symbol").disabled = true;
+}
+
+function game()
+{
+    let position = event.target.id;
+    if(document.getElementById(position).innerHTML != ' ')
+        return;
+    ticTacToe.play(position);
 }
 
 function reset()
@@ -60,15 +71,4 @@ function displayRules()
 function closeRules()
 {
     document.getElementById("list").style.display = "none";
-}
-
-function validate(id){
-    let symbol = document.getElementById(id);
-    player1Symbol = document.getElementById("player1Symbol").value;
-    player2Symbol = document.getElementById("player2Symbol").value
-    if((player1Symbol == player2Symbol) || (['',' '].includes(symbol.value))){
-        symbol.value = '';
-        symbol.focus();
-        document.getElementById("message").innerHTML = "Enter valid symbols";
-    }
 }
